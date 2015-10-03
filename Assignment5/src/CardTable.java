9b1d8aad140c7b5d01217b45cf15bf5354e4e378
jfransen44 <JFransen44@Jeremy-Fransens-MacBook-Pro.local>
2015-10-03 15:45:53 -0700

    updating cardtable
    addle arraySort, valueAsInt, suitAsInt

--- Assignment5/src/CardTable.java
+++ Assignment5/src/CardTable.java
@@ -37,10 +37,9 @@ public class CardTable extends JFrame
    public int getNumPlayers()
    {
       return numPlayers;
-   }//end accessors
+   }//end accessors 
    
-      
-   private static class GUICard
+   static class GUICard
    {
       private static Icon[][] iconCards = new ImageIcon[14][4]; 
       private static Icon iconBack;
@@ -65,5 +64,181 @@ public class CardTable extends JFrame
          iconBack = new ImageIcon("images/BK.gif");
          iconsLoaded = true;
       }
-    }
-}
+     
+      static public Icon getIcon(Card card)
+      {
+
+         if (!iconsLoaded)
+         {
+            loadCardIcons();
+         }
+         
+         int cardValue = valueAsInt(card.getValue());
+         int suitValue = suitAsInt(card.getSuit());
+         
+         return iconCards[cardValue][suitValue];  
+      }//end method getIcon
+      
+      static int valueAsInt(char cardValue)
+      {
+         int value = 0;
+         
+         for (int i = 0; i < Card.cardValue.length; i++)
+            if (cardValue == Card.cardValue[i])
+            {
+               value = i;
+               break;
+            }
+         
+         return value;               
+      }//end method valueAsInt
+      
+      static int suitAsInt(Card.Suit suit)
+      {
+         if (suit.equals("CLUBS"))
+            return 0;
+         else if (suit.equals("DIAMONDS"))
+            return 1;
+         else if (suit.equals("HEARTS"))
+            return 2;
+         else
+            return 4;
+      }//end method suitAsInt
+      
+      
+   }//end class GUICard
+   
+   static class Card
+   {
+      // mappings for valid cards
+      public enum Suit{CLUBS, DIAMONDS, HEARTS, SPADES, X};
+      public static final char[] cardValue = {'A', '2', '3', '4', '5', '6', '7', 
+         '8', '9', 'T', 'J', 'Q', 'K', 'X'};
+      public static char[] valuRanks = {'A', '2', '3', '4', '5', '6', '7', 
+            '8', '9', 'T', 'J', 'Q', 'K', 'X'};
+      
+      private char value;
+      private Suit suit;
+      private boolean errorFlag;
+      
+      /*
+       * constructor with parameters takes a value and Suit
+       * */
+      public Card(char value, Suit suit)
+      {
+         set(value, suit);
+      }
+      
+      /*
+       * default constructor sets value to "A" and suit to spades
+       * */
+      public Card()
+      {
+         set('A', Suit.SPADES);
+      }
+      
+      /*
+       * public method to return value and suit as a string
+       * */
+      public String toString()
+      {
+         if (errorFlag == true)
+            return("[invalid]");
+         return String.valueOf(value) + " of " + suit;
+      }
+      
+      /* 
+       * isValid takes a char value and Suit and
+       * returns true if card value is valid
+       */
+      private boolean isValid(char value, Suit suit)
+      {
+
+         for (int i = 0; i < cardValue.length; i++)
+            if (value == cardValue[i])
+               return true;
+         
+         return false;
+      }
+      
+      /*
+       * set takes a value and Suit, assigns them to a Card and
+       * returns true if successful 
+       */
+      public boolean set(char value, Suit suit)
+      {
+         if (isValid(value, suit))
+         {
+            this.value = value;
+            this.suit = suit;
+            errorFlag = false;
+            return true;
+         }
+         else
+         {
+            errorFlag = true;
+            return false;
+         }
+      }
+      
+      /*
+       * getValue returns a Card's value
+       * */
+      public char getValue()
+      {
+         return value;
+      }
+      
+      /*
+       * getSuit returns a Card's suit
+       * */
+      public Suit getSuit()
+      {
+         return suit;
+      }
+      
+      /*
+       * getErrorFlag returns errorFlag
+       * */
+      public boolean getErrorFlag()
+      {
+         return errorFlag;
+      }
+      
+      /*
+       *equals takes a Card and returns true if it matches the current 
+       *value and Suit
+       */
+      public boolean equals(Card card)
+      {
+         if (card == this)
+            return true;
+         return false;
+      }//end method equal
+      
+      public static void arraySort(Card[] cardArray, int arraySize)
+      {
+         Card tempCard;
+         boolean flag = true;
+         
+         while (flag)
+         {
+            flag = false;
+            for (int i = 0; i < arraySize; i++)
+            {
+               if (GUICard.valueAsInt(cardArray[i].getValue()) >
+               GUICard.valueAsInt(cardArray[i + 1].getValue()))
+               {
+                  tempCard = cardArray[i + 1];
+                  cardArray[i + 1] = cardArray[i];
+                  cardArray[i] = tempCard;
+                  flag = true;
+               }
+            }
+         }
+      }//end method arraySort
+   }//end class Card
+}//end class CardTable
+
+
+
